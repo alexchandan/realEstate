bf5fd1f3f53b25e0409192657cc80fa80b1160ec
chandan <chandanakela321@gmail.com>
2024-04-18 10:53:54 +0530

    Sign-in backend done with jwt token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     try {
@@ -13,5 +14,21 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
         // next(errorHandler(550, "Error from the utils"));
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(404, "Oops..! User not found.")
+        const validPassword = await bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return next(401, "Invalid Password");
+        const token = jwt.sign({ id: validUser._id }, process.env.SECRET_KEY);
+        const { password: pass, ...rest } = validUser._doc;
+
+        res.cookie("access_token", token, { httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60 * 180) }).status(200).json(rest);
+    } catch (error) {
+        next(error);
     }
 }
