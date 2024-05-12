31e8289af865f9ddfdb6c70f58414d2eb296ac93
chandan <chandanakela321@gmail.com>
2024-05-12 16:52:14 +0530

    pre installation setup

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
 import listingRoute from "./routes/listingRoute.js";
 import connectDb from "./db/connectDb.js";
+import path from "path"
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 
 // Database connection
 connectDb(process.env.MONGO_URI);
 
-// app.use(cors());
 
 
 // middleware
@@ -21,12 +22,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use("/api/user", (req, res) => {
-//     res.send("this is res from index.js");
-// });
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/listing", listingRoute)
+
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "disc", "index.html"));
+})
 
 
 // middleware for handling error
